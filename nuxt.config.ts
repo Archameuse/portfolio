f2b3fb86e4cd4c4d6cc4fd8b3f7469b6f70c0f26
Archameuse <mihaildivulin@yandex.ru>
2024-05-20 11:46:59 +0300

    fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/i18n'],
+  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/i18n', "@nuxtjs/google-fonts"],
   i18n: {
     strategy: 'no_prefix',
     defaultLocale: 'en',
     locales: ['en', 'ru', 'ua'],
-  }
+  },
+   googleFonts: {
+    families: {
+      "Kelly Slab": true
+    }
+   }
 })
